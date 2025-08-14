c3c5a1128677310a7f99849bacbcf2a8267704fc
Chieh Shih <cshih685@Chiehs-MacBook-Pro.local>
2025-08-14 00:11:04 -0400

    fetch data, loading page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,11 @@
 import MealsGrid from '@/components/meals/meals-grid';
 import classes from './page.module.css';
 import Link from 'next/link';
+import { getMeals } from '@/lib/meals';
 
-export default function MealsPage() {
+export default async function MealsPage() {
+    // Since Next is React Server Component, we can directly call SQLite
+    const meals = await getMeals();
     return <>
         <header className={classes.header}>
             <h1>
@@ -19,7 +22,7 @@ export default function MealsPage() {
             </p>
         </header>
         <main>
-            <MealsGrid meals={[]} />
+            <MealsGrid meals={meals} />
         </main>
     </>
 }
