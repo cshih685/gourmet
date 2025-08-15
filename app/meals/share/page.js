4f9dba06e9f13191f98040dc96aa5fa3bd48266f
Chieh Shih <cshih685@Chiehs-MacBook-Pro.local>
2025-08-14 22:50:44 -0400

    handle form submission with Server Action

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -2,6 +2,22 @@ import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
+    // By binding with form action, we can collect the form data and take action on the server
+    // Thus, the store of the data on the server side can be triggered when the form is submitted
+    async function shareMeal(formData) {
+        // define it's a "server action"
+        'use server';
+
+        const meal = {
+            title: formData.get('title'),
+            summary: formData.get('summary'),
+            instructions: formData.get('instructions'),
+            image: formData.get('image'),
+            creator: formData.get('name'),
+            creator_email: formData.get('email')
+        }
+        // console.log(meal); //Won't see log on the browser, instead seeing it on the server
+    }
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +27,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +55,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
