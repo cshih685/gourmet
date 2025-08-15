c69f9004273684043730770ae47c19c4ebab750a
Chieh Shih <cshih685@Chiehs-MacBook-Pro.local>
2025-08-14 21:00:11 -0400

    meal details with static value

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,27 @@
+import classes from './page.module.css';
+import Image from 'next/image';
 export default function MealDetailsPage() {
-    return <h1>Meal Details</h1>;
+    const meal = getMeal()
+    return (
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image fill />                    
+                </div>
+                <div className={classes.headerText}>
+                    <h1>TITLE</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${'EMAIL'}`}>NAME</a>
+                    </p>
+                    <p className={classes.summary}>SUMMARY</p>
+                </div>
+            </header>
+            <main>
+                <p className={classes.instructions}
+                dangerouslySetInnerHTML={{
+                    __html: '...',
+                }}></p>
+            </main>
+        </>
+    );
 }
